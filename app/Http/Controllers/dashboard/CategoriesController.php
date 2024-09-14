103cd485a232cedcd2cc13c2571cfa0ad9b446d9
mohame-ali57 <ma6646@fayoum.edu.eg>
2024-09-14 06:46:02 +0300

    add show and edit ,update  functions in crud

--- app/Http/Controllers/dashboard/CategoriesController.php
+++ app/Http/Controllers/dashboard/CategoriesController.php
@@ -37,12 +37,48 @@ class CategoriesController extends Controller
 
     public function edit(string $id)
     {
+        $category=Category::find($id);
+        if($category==null){
+            return view('dashboard.pages.categories.error.page-404');
+        }
 
+        else{
+
+            if(auth()->user()->User_Type=='admin'){
+
+                return view('dashboard.pages.categories.edit', compact('category'));
+
+              }
+
+        else{
+
+            return view('dashboard.pages.categories.error.page-404');
+        }
+
+        }
     }
 
 
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'title'           =>'required|string|unique:categories,title,max:255',
+            'description'     =>'nullable|string|max:1080',
+            'create_user_id'  => 'nullable|exists:users,id',
+            'update_user_id'  => 'nullable|exists:users,id'
+            ]);
+            $category_old=Category::find($id);
+            $category=Category::find($id);
+            if( $category->title== $request->title){
+                $category->title= $category->title;
+            }
+            else{
+                $category->title= $request->title;
+            }
+            $category->description= $request->description;
+            $category->update_user_id=auth()->user()->id;
+            $category->save();
+            return redirect()->route('categories.index')->with('update',"the category {$category_old->title} has been updated");
 
     }
 
